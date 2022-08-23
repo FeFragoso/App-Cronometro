5dfc414273c5d8b0c60e611bc186b4877f456604
fefragoso <felipeofragoso@gmail.com>
2022-08-23 12:15:29 -0300

    Instalação da biblioteca Extend Style Sheet

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image } from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const s = EStyleSheet.create({
+
+})
